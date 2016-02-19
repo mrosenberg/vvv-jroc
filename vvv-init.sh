b0ed193537670a976cbef9067ad8423dcd416250
Matthew Rosenberg <matt@matthewrosenberg.com>
2016-02-18 19:31:44 -0500

    setup for jroc

--- vvv-init.sh
+++ vvv-init.sh
@@ -1,11 +1,11 @@
 # Init script for VVV Auto Bootstrap Demo 1
 
-echo "Commencing VVV Demo 1 Setup"
+echo "Commencing JROC Demo Setup"
 
 # Make a database, if we don't already have one
 echo "Creating database (if it's not already there)"
-mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS vvv_demo_1"
-mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON vvv_demo_1.* TO wp@localhost IDENTIFIED BY 'wp';"
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS jroc_net"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON jroc_net.* TO wp@localhost IDENTIFIED BY 'wp';"
 
 # Download WordPress
 if [ ! -d htdocs ]
@@ -14,11 +14,11 @@ then
 	mkdir htdocs
 	cd htdocs
 	wp core download 
-	wp core config --dbname="vvv_demo_1" --dbuser=wp --dbpass=wp --dbhost="localhost"
-	wp core install --url=vvv-demo-1.dev --title="VVV Bootstrap Demo 1" --admin_user=admin --admin_password=password --admin_email=demo@example.com
+	wp core config --dbname="jroc_net" --dbuser=wp --dbpass=wp --dbhost="localhost"
+	wp core install --url=vvv-demo-1.dev --title="James River Outdoor Coalition" --admin_user=admin --admin_password=password --admin_email=demo@example.com
 	cd ..
 fi
 
 # The Vagrant site setup script will restart Nginx for us
 
-echo "VVV Demo 1 site now installed";
+echo "JROC site now installed";
